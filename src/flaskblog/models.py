182ed93e27705e9bdd11ad595fa654576fac11c6
M-Hamza380 <hamza.anjum380@gmail.com>
2024-09-03 15:51:11 +0500

    update database

--- src/flaskblog/models.py
+++ src/flaskblog/models.py
@@ -2,6 +2,7 @@ from flask import url_for, redirect
 from flask_login import UserMixin
 from flask_admin.contrib.sqla import ModelView
 from itsdangerous import URLSafeTimedSerializer as Serializer
+from datetime import datetime
 
 from src.flaskblog import db, login_manager, app
 
@@ -22,6 +23,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(90), unique=True, nullable=False)
     image_file = db.Column(db.String(90), nullable=False, default='default.jpg')
     password = db.Column(db.String(90), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
 
     def get_reset_token(self, expires_sec=300):
         secret_key = app.config.get('SECRET_KEY')
@@ -40,7 +42,7 @@ class User(db.Model, UserMixin):
         return User.query.get(user_id)
     
     def __repr__(self) -> str:
-        return f"User: ('{self.username}', '{self.email}')"
+        return f"User: ('{self.username}', '{self.email}', '{self.image_file}') "
 
 class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -59,3 +61,15 @@ class RoleAdmin(ModelView):
     column_list = ('id', 'name')
     form_excluded_columns = ('users',)  
 
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __repr__(self) -> str:
+        return f"Post: ('{self.title}', '{self.date_posted}')"
+    
